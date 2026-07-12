28ad314b0529e9269dd472d3a72b29bba43c82ee
王小尽 <1809315974@qq.com>
2026-07-13 01:36:17 +0800

    chore: 更新 GoldenBoughsLib 代码

--- src/main/kotlin/architecture/goldenboughs_lib/util/LazySupplier.kt
+++ src/main/kotlin/architecture/goldenboughs_lib/util/LazySupplier.kt
@@ -1,8 +1,12 @@
 package architecture.goldenboughs_lib.util
 
+import net.minecraft.resources.ResourceLocation
 import java.util.function.Supplier
 
-class LazySupplier<T>(private val factory: () -> T?) : Supplier<T?> {
+class LazySupplier<T>(
+	val id: ResourceLocation,
+	private val factory: () -> T?
+) : Supplier<T?> {
 	var init: Boolean = false; private set
 	private var cached: T? = null
 
